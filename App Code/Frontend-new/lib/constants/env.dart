6c4e34c18c835c1ec3de89e2ddd744abbc7dce87
Hiruni Wijerathna <114440722+HiruniWijerathna@users.noreply.github.com>
2025-06-23 20:43:45 +0530

    add

--- App Code/Frontend-new/lib/constants/env.dart
+++ App Code/Frontend-new/lib/constants/env.dart
@@ -1,5 +1,4 @@
 class ENVConfig {
   // Server Details
-  static const String serverUrl =
-      'https://e42d-2407-c00-d002-af7e-c9cb-242f-23a9-761b.ngrok-free.app';
+  static const String serverUrl = 'https://c090-61-245-169-20.ngrok-free.app';
 }
